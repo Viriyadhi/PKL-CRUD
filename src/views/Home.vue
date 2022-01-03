f4eab9e56aa0e2859858f090882bca081f6146b2
Viriyadhi <viriyadhitjan@gmail.com>
2022-01-03 17:32:32 +0700

    feat:[VS] CRUD

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,103 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <form @submit.prevent="add">
+      <input type="hidden" v-model="form.id" />
+      <input type="text" v-model="form.name" />
+      <button type="submit" v-show="!updateSubmit" v-bind:disabled="buttons">
+        add
+      </button>
+      <button
+        type="button"
+        v-show="updateSubmit"
+        v-bind:disabled="buttons"
+        @click="update(form)"
+      >
+        Update
+      </button>
+    </form>
+    <p>{{ result }}</p>
+    <ul v-for="user in users" :key="user.id">
+      <li>
+        <span>{{ user.name }}</span> &#160; ||
+        <button @click="del(user.id)" v-bind:disabled="buttons">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      buttons: true,
+      result: null,
+      form: {
+        id: "",
+        name: "",
+      },
+      users: null,
+      updateSubmit: false,
+    };
+  },
+  mounted() {
+    this.load();
+  },
+  methods: {
+    load() {
+      axios
+        .get("https://frontend.idaman.co.id/api/profession")
+        .then((res) => {
+          this.users = res.data.data;
+          this.result = res.data.meta.message;
+          setTimeout(() => {
+            this.buttons = false;
+          }, 1000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    add() {
+      axios
+        .post("https://frontend.idaman.co.id/api/profession", this.form)
+        .then(() => {
+          this.load();
+          this.form.name = "";
+        });
+    },
+    edit(user) {
+      this.updateSubmit = true;
+      this.form.id = user.id;
+      this.form.name = user.name;
+    },
+    update(form) {
+      return axios
+        .put("https://frontend.idaman.co.id/api/profession" + form.id, {
+          name: this.form.name,
+        })
+        .then(() => {
+          this.load();
+          this.form.id = "";
+          this.form.name = "";
+          this.updateSubmit = false;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    del(user) {
+      axios
+        .delete("https://frontend.idaman.co.id/api/profession/" + user)
+        .then(() => {
+          this.load();
+        });
+    },
   },
 };
 </script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
